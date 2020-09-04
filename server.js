b3188c40b10bcfe7f086661bf3362c86dd4392c4
andres reyes <andressantiagore@gmail.com>
2020-09-04 18:39:30 -0500

    building the connection

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const helmet = require('helmet')
-// mongoose is a ORM to help with comunicate to database
+const mongoose = require('mongoose')
+
+
 
 const app = express()
 
+mongoose.Promise = global.Promise
+mongoose.connect(
+    process.env.dbURL || 'mongodb://core-db:27017/core',
+    {userNewUrlParses: true})
+
+
 const port = process.env.PORT || 3000
+// mongoose is a ORM to help with comunicate to database
 const server = require('http').Server(app)
 
 server.listen(port)
