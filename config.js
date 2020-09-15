9ec824587714566723cc090425ce627ca3ccdd42
andres reyes <andressantiagore@gmail.com>
2020-09-14 22:50:06 -0500

    make an authentication with twitter

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    twitter: {
+        TWITTER_CONSUMER_KEY: '',
+        TWITTER_CONSUMER_SECRET: '',
+        callbackURL: 'https://core.api.local/api/auth/twitter/callback'
+    }
+
+}
+
+module.exports = config
--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const helmet = require('helmet')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const tokenSecret = process.env.tokenSecret || 'Set the secret key'
+
 
 const morgan = require('morgan')
 
@@ -21,6 +25,23 @@ const server = require('http').Server(app)
 
 app.use(cookieParser())
 app.use(bodyParser.json())
+
+const sessionStore = new MongoStore({
+    mongooseConnection: mongoose.connection
+})
+
+app.use(session({
+    secret: tokenSecret,
+    store: sessionStore,
+    saveUninitialized: false,
+    proxy: true,
+    cookie: {
+      secure: true
+    }
+}))
+
+
+
 // Allow to add a middleware to acces
 app.use(passport.initialize())
 app.use(morgan('dev'))
@@ -28,6 +49,8 @@ app.use(router)
 
 app.use('/api', require('./routes'))
 
-
+app.get('/', (req, res) => {
+    return res.status(200).json({msg: 'Welcome to the api'})
+})
 
 server.listen(port)
